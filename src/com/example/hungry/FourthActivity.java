c2b19fd1677b50598bc20c1b20c2ac0a11b17bb0
Hema <hemalatha.mahesh@2359media.com>
2013-05-25 22:05:01 +0800

    test

--- src/com/example/hungry/FourthActivity.java
+++ src/com/example/hungry/FourthActivity.java
@@ -8,6 +8,6 @@ public class FourthActivity extends FragmentActivity{
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.fourth2);
+		setContentView(R.layout.activity_fourth);
 	}
 }
